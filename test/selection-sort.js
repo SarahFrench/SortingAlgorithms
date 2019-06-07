49e54691972a1ee288f39b1d02c459f71488767f
Sarah French <sarah@sarahfrench.co.uk>
2019-06-07 21:03:25 +0100

    added tests for negative numbers and decimals

--- test/selection-sort.js
+++ test/selection-sort.js
@@ -61,4 +61,22 @@ describe('Selection sort', function () {
        //Assert
        expect(deepEqual(sortedArray,correctAnswer)).to.equal(true)
      });
+  it('should be able to handle negative numbers', function () {
+       // Arrange
+       let array = [-2, -1, 3, 5, -6]
+       let correctAnswer = [-6, -2, -1, 3, 5]
+       //Act
+       let sortedArray = selectionSort(array)
+       //Assert
+       expect(deepEqual(sortedArray,correctAnswer)).to.equal(true)
+     });
+   it('should be able to handle decimal numbers', function () {
+        // Arrange
+        let array = [5, 2, 3, 4, 1.6]
+        let correctAnswer = [1.6, 2, 3, 4, 5]
+        //Act
+        let sortedArray = selectionSort(array)
+        //Assert
+        expect(deepEqual(sortedArray,correctAnswer)).to.equal(true)
+      });
 });
