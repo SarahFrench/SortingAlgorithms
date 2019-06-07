121ebcdb4e0abd25b36b01d31ce2c41381572b29
Sarah French <sarah@sarahfrench.co.uk>
2019-06-07 20:13:09 +0100

    script is verboooooose

--- insertion-sort-console-log.js
+++ insertion-sort-console-log.js
@@ -1,30 +1,37 @@
 const numbers = require('./generate-number-arrays')
 
-let array = [ 3,4,20,20,20,5,1,2 ]
-// let array = numbers.nonUniqueRandomNumberArray(20)
-console.log(array)
+// let array = [4,1,6,2,5,3,9,8,7,10]
+let array = numbers.nonUniqueRandomNumberArray(15)
 
 function insertionSort(array){
+  console.log(array)
   for( i = 0 ; i < array.length; i++ ){
     let currentNumber = array[i];
+    console.log("Currently assessing " + currentNumber + " at position " + i)
     let position = i;
-    console.log(`${currentNumber} is deciding if it should move`)
     for ( position; position >= 0 ; position-- ){
-      swapNumberIfSmallerorEqual(currentNumber, array[position], position)
+      array = swapNumberIfSmallerorEqual(currentNumber, array[position], position, array)
     }
   }
   return array
 }
 
-function swapNumberIfSmallerorEqual(currentNumber, comparisonNumber, comparisonNumberIndex){
+function swapNumberIfSmallerorEqual(currentNumber, comparisonNumber, comparisonNumberIndex , array){
+  console.log ("\t\tLooking at number in position= " + comparisonNumberIndex)
   if (comparisonNumber > currentNumber){
-    newPosition = comparisonNumberIndex
-    oldPosition = array.indexOf(currentNumber)
+    newPosition = comparisonNumberIndex;
+    oldPosition = comparisonNumberIndex+1;
     array[oldPosition] = comparisonNumber;
     array[newPosition] = currentNumber;
-    console.log(`${comparisonNumber}> ${currentNumber} so ${currentNumber} moved to the left by 1`)
-    console.log(`\t\t${array}`)
+    console.log(`\t** ${comparisonNumber} > ${currentNumber} so ${currentNumber} moved to the left by 1`)
+    console.log("\t" +array)
   }
+  return array
 }
 
-console.log(insertionSort(array))
+insertionSort(array);
+
+module.exports = {
+  insertionSort,
+  swapNumberIfSmallerorEqual
+}
