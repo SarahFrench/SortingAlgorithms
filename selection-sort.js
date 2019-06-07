7786bf9bb21fabd8ec8f3baeaa1cf8f78d9843f9
Sarah French <sarah@sarahfrench.co.uk>
2019-06-07 14:08:13 +0100

    Tidied up version of selection sort, could probably be refactored further

--- selection-sort.js
+++ selection-sort.js
@@ -1,43 +1,34 @@
 const numbers = require('./generate-number-arrays')
 
-let array = numbers.nonUniqueRandomNumberArray(1,20,1)
-console.log(array)
+function selectionSort(array){
+  let counter = 0;
 
-let length = array.length;
+  array.forEach( firstUnsortedNumber => {
+    lowestNumber = firstUnsortedNumber;
+    lowestNumberIndex = 0;
 
-let counter = 0;
-
-array.forEach( number => {
-  lowestNumber = number;
-  lowestNumberIndex = 0;
-
-  for(i=0+counter; i<length; i++){
-    if (array[i] < lowestNumber){
-      lowestNumber = array[i];
-      lowestNumberIndex = i;
+    for(i=0+counter; i<array.length; i++){ //counter variable makes new for loop begin at the start of "unselected" elements towards the end of the array
+      if (array[i] < lowestNumber){
+        lowestNumber = array[i];
+        lowestNumberIndex = i; //record this so can swap
+      }
     }
-  }
 
-  array[counter] = lowestNumber;
-  if (number != lowestNumber){
-    array[lowestNumberIndex] = number;
-  }
-  counter += 1;
-})
+    if (firstUnsortedNumber != lowestNumber){
+      array[counter] = lowestNumber;
+      array[lowestNumberIndex] = firstUnsortedNumber;
+    }
 
+    counter += 1;
+  })
+  return array
+}
+
+// let array = [ 4, 15, 16, 15, 9, 0, 11, 8, 9, 2, 4, 2, 15, 17, 3, 17, 2, 0, 11, 8 ]
+let array = numbers.nonUniqueRandomNumberArray(20)
+
+console.log("\nUnsorted array:")
 console.log(array)
 
-
-
-
-
-// let first = array[0]
-// console.log(first)
-//
-// array.forEach( number => {
-//   if (number < first) {
-//     first = number
-//   }
-// })
-//
-// console.log(first)
+console.log("\nSelection sorted array:")
+console.log(selectionSort(array))
