0b791eb7da02ff93cb350de6cb9341b971433f30
Sarah French <sarah@sarahfrench.co.uk>
2019-06-07 13:53:42 +0100

    Can sort arrays of numbers into ascending order

--- selection-sort.js
+++ selection-sort.js
@@ -1,16 +1,43 @@
 const numbers = require('./generate-number-arrays')
 
-let array = [ 9, 8, 6, 4, 1, 0, 5, 8, 4, 1 ]
+let array = numbers.nonUniqueRandomNumberArray(1,20,1)
+console.log(array)
 
-let length = array.length
+let length = array.length;
 
-let first = array[0]
-console.log(first)
+let counter = 0;
 
 array.forEach( number => {
-  if (number < first) {
-    first = number
+  lowestNumber = number;
+  lowestNumberIndex = 0;
+
+  for(i=0+counter; i<length; i++){
+    if (array[i] < lowestNumber){
+      lowestNumber = array[i];
+      lowestNumberIndex = i;
+    }
   }
+
+  array[counter] = lowestNumber;
+  if (number != lowestNumber){
+    array[lowestNumberIndex] = number;
+  }
+  counter += 1;
 })
 
-console.log(first)
+console.log(array)
+
+
+
+
+
+// let first = array[0]
+// console.log(first)
+//
+// array.forEach( number => {
+//   if (number < first) {
+//     first = number
+//   }
+// })
+//
+// console.log(first)
