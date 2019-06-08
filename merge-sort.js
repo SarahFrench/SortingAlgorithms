96edc7da7e03e72b613216ace1fd763837bb125c
Sarah French <sarah@sarahfrench.co.uk>
2019-06-08 20:28:03 +0100

    I CAN MAKE BINARY TREES Recusion is hard y'all

--- merge-sort.js
+++ merge-sort.js
@@ -1,23 +1,32 @@
 const numbers = require('./generate-number-arrays')
 
-let array = numbers.nonUniqueRandomNumberArray(10);
-console.log(array)
+// let array = numbers.nonUniqueRandomNumberArray(100);
+let array = [ 8, 12, 12, 24, 16, 15, 6 ]
 
-function splitArrayInTwo(array){
-  output = [];
-  let midpoint = Math.floor(array.length/2);
-  if (array.length > 1 && midpoint != 0){
-    let midpoint = Math.floor(array.length/2);
-    let array1 = array.slice(0, midpoint);
-    let array2 = array.slice(midpoint, array.length);
-    output = [array1, array2];
-  }
-  return output
+function printArray(array) {
+    for (var i = 0; i < array.length; i++)
+        if (Array.isArray(array[i]))
+            printArray(array[i])
+        else
+            console.log(array[i])
 }
 
-array = splitArrayInTwo(array)
-array.forEach ( function (x, i){
-  array[i] = splitArrayInTwo(x)
-})
+function makeBinaryTree(array){
+  if (array.length > 1){
+    array = splitArrayInTwo(array);
+    for (var i = 0; i < array.length; i++){
+      array[i] = makeBinaryTree(array[i]);
+    }
+  }
+  return array
+}
 
-console.log(array)
+function splitArrayInTwo(array){
+  let midpoint = Math.floor(array.length/2);
+  let array1 = array.slice(0, midpoint);
+  let array2 = array.slice(midpoint, array.length);
+  array = [array1, array2]
+  return array;
+}
+
+console.log(makeBinaryTree(array))
