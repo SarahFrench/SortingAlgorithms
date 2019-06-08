ce8fa842148ef10a97e6dfaa521cd110a71670b2
Sarah French <sarah@sarahfrench.co.uk>
2019-06-08 01:49:40 +0100

    moving towards splitting array correctly, into tree, for merge sort

--- merge-sort.js
+++ merge-sort.js
@@ -1,10 +1,23 @@
 const numbers = require('./generate-number-arrays')
 
-let array = numbers.nonUniqueRandomNumberArray(5);
+let array = numbers.nonUniqueRandomNumberArray(10);
 console.log(array)
 
-let listOfLists = []
+function splitArrayInTwo(array){
+  output = [];
+  let midpoint = Math.floor(array.length/2);
+  if (array.length > 1 && midpoint != 0){
+    let midpoint = Math.floor(array.length/2);
+    let array1 = array.slice(0, midpoint);
+    let array2 = array.slice(midpoint, array.length);
+    output = [array1, array2];
+  }
+  return output
+}
 
-array.forEach( number => {
-  listOfLists.push([number])
+array = splitArrayInTwo(array)
+array.forEach ( function (x, i){
+  array[i] = splitArrayInTwo(x)
 })
+
+console.log(array)
